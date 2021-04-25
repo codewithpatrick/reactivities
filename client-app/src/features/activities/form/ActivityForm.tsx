5f3471d29431955c29e207d0b639246ac8dffb14
Ppangilinan <Ppangilinan@DESKTOP-TOUPA8K>
2021-04-25 20:28:38 +0800

    Implement API CRUD for Activities and Enable Loading Indicator

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,12 +6,14 @@ interface Props {
   activity: Activity | undefined;
   closeForm: () => void;
   createOrEdit: (activity: Activity) => void;
+  submitting: boolean;
 }
 
 export default function ActivityForm({
   activity: selectedActivity,
   closeForm,
   createOrEdit,
+  submitting,
 }: Props) {
   const initialState = selectedActivity ?? {
     id: "",
@@ -59,6 +61,7 @@ export default function ActivityForm({
           onChange={hanldeInputChange}
         ></Form.Input>
         <Form.Input
+          type="date"
           placeholder="Date"
           value={activity.date}
           name="date"
@@ -81,6 +84,7 @@ export default function ActivityForm({
           positive
           type="submit"
           content="Submit"
+          loading={submitting}
         ></Button>
         <Button
           onClick={closeForm}
